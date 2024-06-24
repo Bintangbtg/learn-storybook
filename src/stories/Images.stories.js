64bbbe50040c9429ec0d350189d15655cc714ade
Bintangbtg <112920061+Bintangbtg@users.noreply.github.com>
2024-06-24 14:57:31 +0700

    first commit

---
+++ src/stories/Images.stories.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Images from '../components/Image/Images';
+
+export default {
+  title: 'Example/Images',
+  component: Images,
+  argTypes: {
+    size: {
+      control: {
+        type: 'select',
+        options: ['small', 'medium', 'large'],
+      },
+    },
+  },
+};
+
+const Template = (args) => <Images {...args} />;
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+  size: 'medium',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+};
